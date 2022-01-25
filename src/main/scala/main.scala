3f988398849df674e09510e0f4085a14715eebc8
revaleo <leo.chen@revature.net>
2022-01-24 21:31:55 -0800

    4.0 creating/deleting/updating done. All done.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -8,17 +8,17 @@ The functionality of this program is that, it will ask for user/pass for login, 
  covid-19 statistics and do calculation.
 
 Referenced data:
-
 https://github.com/owid/covid-19-data/blob/master/public/data/latest/owid-covid-latest.csv
 
+Program github link:
+https://github.com/revaleo/project0/blob/master/src/main/scala/main.scala
 * */
-import main.connection
 
 import java.lang.Math.round
 import java.sql.{Connection, DriverManager}
 import scala.Console._
-import scala.util.Random
 import scala.io.StdIn.readLine
+import scala.collection.mutable.ListBuffer
 
 
 object main extends App {
@@ -46,7 +46,7 @@ def auth():Boolean={
    else
   false
 }
-  //Retrieving total covid cases method
+  //Retrieving total covid cases method (option 1)
   def totalcases():Unit={
     var loop2 = 1
    while (loop2==1) {
@@ -88,14 +88,14 @@ def auth():Boolean={
              |
              |""".stripMargin)
          while (resultQuery.next()) {
-           println("The worldwide total cases is:" + " " + resultQuery.getString("total"))
+           println("The worldwide total cases is:" + f"${CYAN_B}${BLACK} " + resultQuery.getString("total")+f"${RESET}")
          }
          println("Do you want to continue(1), or go back to the previous menu(2)?")
          loop2 = readLine().toInt
      }
    }
   }
-  // vaccine and death method
+  // vaccine and death method (option2)
   def vacdeath():Unit= {
     var loop3 = 1
     while (loop3 == 1) {
@@ -170,6 +170,59 @@ def auth():Boolean={
       }
     }
   }
+  //truncating and creating table
+  def copytable():String= {
+    println("Please enter the new table name:")
+    val table = readLine()
+    connection.createStatement().execute(
+      s"CREATE TABLE $table AS SELECT location, total_cases, population FROM covidstat;")
+    println(s"\nNew table $table created with only country, total cases and population.")
+    println("All records were copied from original statistic table!")
+    table
+  }
+  //Update/delete record
+  def updatetable(s:String):Unit={
+    println("Please enter the location you what to delete:")
+      val loca = readLine()
+      val deletestring = s"""delete from $s where location = "$loca""""
+      //var update = s"update $s set $column= ? where ?;"
+     connection.prepareStatement(deletestring).execute()
+   // insert.setInt(1,1)
+   // insert.setInt(2,2)
+    //insert.setInt(3,3)
+   // insert.execute()
+    println(s"$loca deleted!")
+      val resultQuery = connection.createStatement().executeQuery("Select round(100*sum(total_cases)/sum(population),2) as a from covidstat")
+    while(resultQuery.next()){
+      println("World total infection rate BEFORE deletion is:" +f"${CYAN} "+ resultQuery.getString("a")+s"${RESET}%")
+    }
+      val resultQuery2 = connection.createStatement().executeQuery(s"Select round(100*sum(total_cases)/sum(population),2)as a from $s")
+      while(resultQuery2.next()){
+        println("World total infection rate AFTER deletion is:" + f"${CYAN} "+ resultQuery2.getString("a")+s"${RESET}%")
+      }
+    Thread.sleep(5000)
+  }
+  def updatetable2(s:String):Unit={
+    println("Please enter the location you what to update:")
+    val loca = readLine()
+    println("Please enter the total cases you wish to change to:")
+    val cases= readLine()
+    val updatestring = s"""update $s set total_cases = ? where location = "$loca""""
+
+    val update=connection.prepareStatement(updatestring)
+      update.setString(1,cases)
+      update.execute()
+        println(s"$loca updated!")
+    val resultQuery = connection.createStatement().executeQuery("Select round(100*sum(total_cases)/sum(population),2) as a from covidstat")
+    while(resultQuery.next()){
+      println("World total infection rate BEFORE update is:" +f"${CYAN} " + resultQuery.getString("a")+s"${RESET}%")
+    }
+    val resultQuery2 = connection.createStatement().executeQuery(s"Select round(100*sum(total_cases)/sum(population),2)as a from $s")
+    while(resultQuery2.next()){
+      println("World total infection rate AFTER update is:" +f"${CYAN} " + resultQuery2.getString("a")+s"${RESET}%")
+    }
+  Thread.sleep(5000)
+  }
 
 
   //The beginning of the program
@@ -184,18 +237,20 @@ def auth():Boolean={
     println("Now please choose what topic about covid you are interested in:\n")
     println("1. Total worldwide cases as of 1/18/2022.\n")
     println("2. Vaccination and death Search by country.\n")
-    println("3. \n")
-    println("4. To end the program.\n")
+    println("3. Truncating and copying original data into a new table.\n")
+    println("4. Deleting country to compare world infection rate.\n")
+    println("5. Updating country to compare world infection rate.\n")
+    println("6. To end the program.\n")
   var i= readLine().toInt
     i match {
       case 1 =>totalcases()
-
       case 2 =>vacdeath()
-
-      //    case 3 =>
-      case 4 => loop1 = 0
+      case 3 =>copytable()
+      case 4 =>updatetable(copytable)
+      case 5 =>updatetable2(copytable)
+      case 6 => loop1 = 0
         connection.close()
-        println("Thanks for using this program, good bye!")
+        println(f"${YELLOW_B}${BLACK}Thanks for using this program, good bye!${RESET}")
     }
   }
 
